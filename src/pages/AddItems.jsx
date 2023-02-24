297076f4a6fd39faf30403ae813015fae95ae69f
castild2 <castild2@lsbu.ac.uk>
2023-02-24 17:36:28 +0000

    update image icon and background color

--- src/pages/AddItems.jsx
+++ src/pages/AddItems.jsx
@@ -1,8 +1,55 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 
 const AddItems = () => {
+
+  const [ infoImage, setInfoImage ] = useState("")
+
+  const onSubmit =  (e) => {
+    e.preventDefault()
+    const formData = new FormData(e.target)
+    const imageFile = formData.get('img');
+
+    const image = new Image();
+    image.src = URL.createObjectURL(imageFile);
+
+    image.onload = async () => {
+      const canvas = document.createElement('canvas');
+      canvas.width = 128;
+      canvas.height = 128;
+      const ctx = canvas.getContext('2d');
+      ctx.drawImage(image, 0, 0, 128, 128);
+
+      const formData = new FormData();
+      formData.append('image', canvas.toDataURL('image/jpeg'));
+
+      const img = formData.get('image').split(",")[1]
+
+      const options = {
+        method: 'POST',
+        headers: { 'content-type': 'application/x-www-form-urlencoded' },
+        data: { image : img },
+        url: "https://5cc3-2a00-23c8-131a-f901-d90e-9128-5dad-a78b.ngrok.io/predict/"
+      };
+
+       const resServer = await axios(options)
+
+       setInfoImage(resServer.data.class)
+    }
+
+  }
+
   return (
-    <div>AddItems</div>
+    <div>
+      <form onSubmit={onSubmit}  >
+         <input type="file" name="img" accept='.jpeg, .jpg' />
+         <button>Send</button>
+         <h1>The cake is:</h1>
+         <h3>{infoImage}</h3>
+      </form>
+
+      
+    </div>
   )
 }
 
