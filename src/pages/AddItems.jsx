b6d9bb432a3b01c29df47557ec1d1125994ac9ff
castild2 <castild2@lsbu.ac.uk>
2023-02-27 18:07:18 +0000

    adding new animations

--- src/pages/AddItems.jsx
+++ src/pages/AddItems.jsx
@@ -1,56 +1,145 @@
-import axios from 'axios';
-import React, { useState } from 'react'
+import axios from "axios";
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import styled from "styled-components";
+import AddFile from "../animations/AddFile";
+import Loading from "../animations/Loading";
 
 const AddItems = () => {
+  const [infoImage, setInfoImage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [img, setImg] = useState("");
 
-  const [ infoImage, setInfoImage ] = useState("")
-
-  const onSubmit =  (e) => {
-    e.preventDefault()
-    const formData = new FormData(e.target)
-    const imageFile = formData.get('img');
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true)
+    const formData = new FormData(e.target);
+    const imageFile = formData.get("img");
 
     const image = new Image();
     image.src = URL.createObjectURL(imageFile);
 
     image.onload = async () => {
-      const canvas = document.createElement('canvas');
+      const canvas = document.createElement("canvas");
       canvas.width = 128;
       canvas.height = 128;
-      const ctx = canvas.getContext('2d');
+      const ctx = canvas.getContext("2d");
       ctx.drawImage(image, 0, 0, 128, 128);
 
       const formData = new FormData();
-      formData.append('image', canvas.toDataURL('image/jpeg'));
+      formData.append("image", canvas.toDataURL("image/jpeg"));
 
-      const img = formData.get('image').split(",")[1]
+      const img = formData.get("image").split(",")[1];
 
       const options = {
-        method: 'POST',
-        headers: { 'content-type': 'application/x-www-form-urlencoded' },
-        data: { image : img },
-        url: "https://b2bd-2a00-23c8-131a-f901-9987-fae6-5a27-e76.ngrok.io/predict/"
+        method: "POST",
+        headers: { "content-type": "application/x-www-form-urlencoded" },
+        data: { image: img },
+        url: "https://b2bd-2a00-23c8-131a-f901-9987-fae6-5a27-e76.ngrok.io/predict/",
       };
 
-       const resServer = await axios(options)
+      const resServer = await axios(options);
 
-       setInfoImage(resServer.data.class)
-    }
+      setImg(formData.get("image"));
+      setIsLoading(false)
 
-  }
+      setInfoImage(resServer.data.class);
+    };
+  };
 
   return (
-    <div>
-      <form onSubmit={onSubmit}  >
-         <input type="file" name="img" accept='.jpeg, .jpg' />
-         <button>Send</button>
-         <h1>The cake is:</h1>
-         <h3>{infoImage}</h3>
-      </form>
+    <MainPrincipal>
+      <Form onSubmit={onSubmit}>
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <>
+          
+            <MainContainerAddImg>
+              <h1>Add image</h1>
+              <AddFile />
+              <InputFile type="file" name="img" accept=".jpeg, .jpg" />
+            </MainContainerAddImg>
+            <button>Send</button>
+          </>
+        )}
+      </Form>
 
-      
-    </div>
-  )
-}
+      {/* <form action="">
+        <div>
+          <label htmlFor="">Expire Date</label>
+          <input type="date" />
+        </div>
 
-export default AddItems
+        <img src={img} alt="" />
+  </form>*/}
+    </MainPrincipal>
+  );
+  /*class AddItems extends Component {
+    sate={
+      date: new Date("2023","02","27")
+    }
+    render (){
+      return(
+        <>
+        <div className='container'>
+
+        <div className='center'>
+          <DatePicker selected={this.state.date}></DatePicker>
+
+        </div>
+
+          </div>
+        </>
+      )
+    }
+  }*/
+};
+
+export default AddItems;
+
+const MainPrincipal = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 90vh;
+`;
+
+const MainContainerAddImg = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Form = styled.form`
+  background-color: white;
+  border-radius: 10px;
+  width: 90%;
+  padding: 20px 0;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  button {
+    width: 80%;
+    padding: 10px;
+    background-color: #8b0505;
+    color: white;
+    border: none;
+    font-size: 18px;
+    font-weight: 500;
+    border-radius: 5px;
+  }
+`;
+
+const InputFile = styled.input`
+  opacity: 0;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  z-index: 2;
+  position: absolute;
+`;
