5f738cb570f3ddcd7fa3dbfd530617821ab399ad
castild2 <castild2@lsbu.ac.uk>
2023-05-10 22:40:01 +0100

    Explanation of the app

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,9 +5,19 @@ const Home = () => {
 
   return (
     <MainContainer>
-      <TitleHome>LIST OF PRODUCTS TO EXPIRE</TitleHome>
+      <TitleHome>Day Dot-Cake</TitleHome>
       <MainBody>
-      <h2>Titulo Aplicacion</h2>
+      <h2>Creating a web application to identify cakes using a Convolutional Neaural Network (CNN) 
+        model for a coffee shop can be a great way to streamline the process of managing inventory 
+        and ensuring the freshness of the baked goods. By using image recognition technology, employees 
+        can simply take a photo of the cake, and the system can identify it using the pre-trained CNN 
+        model. Additionally, the application can integrate with a database to track the expiration dates 
+        of the cakes and show the staff when the cakes are approaching their ecpiration dates. This way,
+        the coffee shop can ensure that the cakes are always fresh and minimize waste by only keeping the 
+        cakes on display that are still within their expiration dates. Overall, this web application can 
+        provide a convenient and efficient solution to manage the inventory and freshness of cakes for a 
+        coffee shop. 
+      </h2>
 
       </MainBody>
     </MainContainer>
